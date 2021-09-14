8bc82227fef295965fd0c1ca53b90484512c9a0e
Eduardo <e.laboral@xor.cl>
2021-09-13 23:56:19 -0300

    return response in api endpoint

--- casexample/views.py
+++ casexample/views.py
@@ -1,10 +1,13 @@
+from django.http.request import HttpHeaders
 from oauth2_provider.views.generic import ProtectedResourceView
 from django.core import serializers
-from django.http import HttpResponse
-import json
+from django.http import response
 
 
 class ApiEndpoint(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
-        self.head
-        return serializers.serialize("json", [request.user])[1:-1]
+        resp = response.HttpResponse(
+            serializers.serialize("json", [request.user])[1:-1]
+        )
+        resp.headers["content-type"] = "application/json"
+        return resp
